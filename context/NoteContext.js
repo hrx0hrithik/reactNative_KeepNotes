0fe4e888b05c203803f5c4be0ecb863ec62826e2
Hrithik Gupta <hrithikgupta52@gmail.com>
2024-05-08 18:36:53 +0530

    Update NoteContext.js

--- context/NoteContext.js
+++ context/NoteContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { formattedDateNTime } from "../utility/dates";
 import { storage } from "../utility/mmkv";
 
